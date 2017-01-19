b7d820fc6bc8a63fafc829521bb2216389d23e41
Luis Briceño <luisabc132@gmail.com>
2017-01-19 19:00:25 +0100

    seed and DB

--- auction/db/seeds.rb
+++ auction/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Project.create!(name: 'Envelat', description: 'Diseño Identidad')
+# Project.create!(name: 'Tikets', description: 'Mantenimiento de diseño')
+# Project.create!(name: 'Estupendu', description: 'Diseño Web')
+User.create!(name: 'luis', email:'luis@luis.com')
+User.create!(name: 'carlos', email:'carlos@gmail.com')
+User.create!(name: 'daniel', email:'daniel@hotmail.com')
+User.create!(name: 'jose', email:'jose@yahoo.com')
+User.create!(name: 'maria', email:'maria@tmt.com')
